792d7d1b57a958034b4fceaaf4e7df022a8271d3
Jakob Sturla <jseinarsson@gmail.com>
2020-10-05 23:03:38 +0000

    Working on dashboard, text input currently working, loading animation commented out

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 const loading = document.querySelector(".loading");
-const application = document.querySelector(".application");
+const dashboard = document.querySelector(".dashboard");
 
-setTimeout(function(){
+/* setTimeout(function(){
     loading.classList.add("hidden");
     loading.classList.remove("loading");
-    application.classList.remove("hidden");
-}, 10500);
+    dashboard.classList.remove("hidden");
+    dashboard.classList.add("reveal");
+}, 10500); */
+
+const nameBtn = document.querySelector(".name__button");
+const nameInput = document.querySelector(".name__textbox");
+const nameOutput = document.querySelector(".name__output");
+
+nameBtn.addEventListener('click', function(){
+    // Create input value text node.
+    const  nameInputContent = document.createTextNode(nameInput.value);
+    // Add text node to <span> with class ".name__output" (replacing existing node)
+    nameOutput.replaceChild(nameInputContent, nameOutput.lastChild);
+});
--- readme.md
+++ readme.md
@@ -2,4 +2,6 @@
 
 [See the site live here](http://)
 
-1. Loading screen using CSS animations & setTimeout() in JS to delay the reveal of the main section.
+1. This site has a pseudo loading screen using CSS animations & setTimeout() in JS to delay the reveal of the main section for 10.5 seconds. Ideally, the loading of the page would control the CSS animation, but I decided to fake it for the purpose of this project, especially since the loading time won't be very long.
+
+2. The Dashboard is revealed once the setTimeout counter runs out. 
