937f9a6985095d2d358e20d5187981a59eabf087
Jakob Sturla <jseinarsson@gmail.com>
2020-10-06 23:08:41 +0000

    Finished layout of second scene

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const loading = document.querySelector(".loading");
 const dashboard = document.querySelector(".dashboard");
 
-/* setTimeout(function(){
+setTimeout(function(){
     loading.classList.add("hidden");
     loading.classList.remove("loading");
     dashboard.classList.remove("hidden");
     dashboard.classList.add("reveal");
-}, 10500); */
+}, 10500);
 
 const nameBtn = document.querySelector(".name__button");
 const nameInput = document.querySelector(".name__textbox");
@@ -17,4 +17,6 @@ nameBtn.addEventListener('click', function(){
     const  nameInputContent = document.createTextNode(nameInput.value);
     // Add text node to <span> with class ".name__output" (replacing existing node)
     nameOutput.replaceChild(nameInputContent, nameOutput.lastChild);
+
+    //
 });
